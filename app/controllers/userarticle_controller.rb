18ec6b52d4fbe079150cd08fd404fe2f96ef753e
Vicente-jpro <vicenteviciii@outlook.com>
2021-12-11 16:05:29 +0100

    Pagination added

--- app/controllers/userarticle_controller.rb
+++ app/controllers/userarticle_controller.rb
@@ -1,6 +1,6 @@
 class UserarticleController < ApplicationController
  
   def show
-    @user_articles = Article.all.where(user_id:params[:id])
+    @user_articles = Article.all.where(user_id:params[:id]).paginate(page: params[:page], per_page: 2)
   end
 end

ca951744a9201af20405239946eff686407f53b6
Vicente-jpro <vicenteviciii@outlook.com>
2021-12-16 18:25:06 +0100

    Relation many to many
    Article with Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_14_052124) do
+ActiveRecord::Schema.define(version: 2021_12_16_170236) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"

b6dd44666a1be3e9a6ceba59b1423e2e5eee94b1
Vicente-jpro <vicenteviciii@outlook.com>
2021-12-25 17:56:37 +0100

    User authenticatication categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, :set_category, only: %i[ show edit update destroy ]
 
   # GET /categories or /categories.json
   def index

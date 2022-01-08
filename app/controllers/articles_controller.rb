f9ee09535c347da47ebbc04d21c438df4d9e8197
vicente-jpro <vicenteviciii@outlook.com>
2022-01-08 23:00:48 +0100

    Dependent destroy: article, articlesCategory

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -66,7 +66,7 @@ class ArticlesController < ApplicationController
 
   # DELETE /articles/1 or /articles/1.json
   def destroy 
-    if permit_user_modify_post
+    if permition_user_modify_post
       @article.destroy
       respond_to do |format|
         format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
 
-	has_and_belongs_to_many :articles, join_table: 'articles_categories'
+	has_and_belongs_to_many :articles, dependent: :destroy, join_table: 'articles_categories'
 
 	validates :name, presence: true, uniqueness:true, length:{ minimum: 3, maximum: 30 }
 end
